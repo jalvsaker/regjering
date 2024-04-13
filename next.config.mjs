9a08ccf4e91c780fc4cb467de61624696c3c24cf
jalvsaker <jalvsaker@users.noreply.github.com>
2024-04-14 01:04:10 +0200

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: { remotePatterns: [{ hostname: "data.stortinget.no" }] },
+};
 
 export default nextConfig;
