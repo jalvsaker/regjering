73cd495cc67aefbf21cb3c6165ae3f15c529f758
jalvsaker <jalvsaker@users.noreply.github.com>
2024-04-14 12:05:24 +0200

    show age and departement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,12 @@ interface Regjeringsmedlem {
   fornavn: string;
   etternavn: string;
   foedselsdato: string;
+  kjoenn: number;
   tittel: string;
+  verv: string;
+  departement: string;
   parti: {
+    id: string;
     navn: string;
   };
 }
@@ -24,43 +28,59 @@ function parseDate(date: string) {
   return new Date(timestamp + timezoneOffset * 60000);
 }
 
+function calculateAge(dateOfBirth?: Date) {
+  if (!dateOfBirth) return;
+  const today = new Date();
+  const age = today.getUTCFullYear() - dateOfBirth.getUTCFullYear();
+  const m = today.getUTCMonth() - dateOfBirth.getUTCMonth();
+  if (m < 0 || (m === 0 && today.getUTCDate() < dateOfBirth.getUTCDate())) {
+    return age - 1;
+  }
+  return age;
+}
+
 export const metadata: Metadata = {
-  title: "Hvem er i regjering?",
+  title: "Hvem er i regjeringen?",
 };
 
 export default async function Home() {
   const res = await fetch(
     "https://data.stortinget.no/eksport/regjering?format=json",
-    { next: { revalidate: 86400 } },
+    { next: { revalidate: 3600 } },
   ).then((res) => res.json());
 
   const medlemer: Regjeringsmedlem[] = res.regjeringsmedlemmer_liste;
 
   return (
     <div className="mx-auto container max-w-4xl">
-      <h1 className="text-4xl text-center m-8">Hvem er i regjering?</h1>
+      <h1 className="text-4xl text-center m-8">Hvem er i regjeringen?</h1>
       <div className="grid m-4 gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-        {medlemer.map((m) => (
-          <div key={m.id} className="flex flex-col text-center">
-            <Image
-              src={`https://data.stortinget.no/eksport/personbilde?personid=${m.id}&storrelse=stort&erstatningsbilde=true`}
-              width={500}
-              height={668}
-              alt={`${m.fornavn} ${m.etternavn}`}
-              className="self-center"
-            />
-            <div className="text-lg">
-              {m.fornavn} {m.etternavn}
+        {medlemer.map((m) => {
+          const dateOfBirth = parseDate(m.foedselsdato);
+          return (
+            <div key={m.id} className="flex flex-col text-center">
+              <Image
+                src={`https://data.stortinget.no/eksport/personbilde?personid=${m.id}&storrelse=stort&erstatningsbilde=true`}
+                width={500}
+                height={668}
+                alt={`${m.fornavn} ${m.etternavn}`}
+                className="self-center"
+              />
+              <div className="text-lg">
+                {m.fornavn} {m.etternavn}
+              </div>
+              <div>
+                {dateOfBirth?.toLocaleString("no", {
+                  dateStyle: "medium",
+                })}{" "}
+                ({calculateAge(dateOfBirth)})
+              </div>
+              <div>{m.tittel}</div>
+              <div className="text-xs">{m.departement}</div>
+              <div className="text-sm">{m.parti.navn}</div>
             </div>
-            <div>
-              {parseDate(m.foedselsdato)?.toLocaleString("no", {
-                dateStyle: "medium",
-              })}
-            </div>
-            <div>{m.tittel}</div>
-            <div className="text-sm">{m.parti.navn}</div>
-          </div>
-        ))}
+          );
+        })}
       </div>
       <div className="text-center m-2">
         Sist oppdatert:{" "}
